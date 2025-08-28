bd3b87cb9566ec828c74109dda48ff0f6f218fa8
martijnooo <martijnbalder@gmail.com>
2025-08-28 16:05:22 +0200

    Default starting page added

---
+++ Home.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import streamlit as st
+
+st.set_page_config(
+    page_title="Themed App",
+    page_icon="🎨",
+    layout="wide"
+)
+
+st.image("assets\Home_movie.png")
+
+st.title("Movie Recommender", width="stretch")
+
